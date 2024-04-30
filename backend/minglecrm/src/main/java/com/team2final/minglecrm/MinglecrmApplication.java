3ce719d7b4ed0abb5a0bbc3aa615366f3ca1574c
jmh <thinghoon@icloud.com>
2024-04-30 15:31:56 +0900

    백엔드 초기설정

---
+++ backend/minglecrm/src/main/java/com/team2final/minglecrm/MinglecrmApplication.java
@@ -0,0 +1,13 @@
+package com.team2final.minglecrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinglecrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinglecrmApplication.class, args);
+	}
+
+}
