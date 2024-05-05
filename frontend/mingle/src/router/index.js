52f7789af4a38f1d7ede7c5c5de14acebe74b985
chan-nni <rkdcksal02@gmail.com>
2024-05-06 01:12:01 +0900

    router add

---
+++ frontend/mingle/src/router/index.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import SignUpPage from "@/views/SignUpPage.vue";
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/signup', // 회원가입 페이지의 경로
+    name: 'SignUp',
+    component: SignUpPage // 회원가입 페이지 컴포넌트
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
