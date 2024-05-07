61d29e712f89ff690426fba492287534373d6d9f
jmh <thinghoon@icloud.com>
2024-05-07 12:40:48 +0900

    customer setter, 연관관계 매핑 수정

--- backend/minglecrm/src/main/java/com/team2final/minglecrm/domain/Customer.java
+++ backend/minglecrm/src/main/java/com/team2final/minglecrm/domain/Customer.java
@@ -25,7 +25,7 @@ public class Customer extends BaseTimeEntity {
     private String phone;
     private String address;
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", referencedColumnName = "id")
     private User user;
 
